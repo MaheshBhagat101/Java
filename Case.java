33c274de8273dfbc3a784799f1494bf0e2758971
MaheshBhagat101 <149859529+MaheshBhagat101@users.noreply.github.com>
2024-03-07 14:48:32 +0000

    Case

---
+++ Case.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Case{
+    public static void main(String[]args)
+
+    {
+        Scanner in = new Scanner(System.in);
+
+        char ch = in.next().trim().charAt(0); //trim will reduce the extra space on in the character
+                                                    // charAt(0), here 0 represents the index number of the 1st char
+        if(ch >= 'a' && ch <= 'z')
+        {
+            System.out.println("lowercase");
+
+        }
+        else
+        {
+            System.out.println("uppercase");
+        }
+
+
+    }
+    
+}
